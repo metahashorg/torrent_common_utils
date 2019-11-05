9d07a822c1d1e2f433f781449e81b175758121ae
user <sv_91@inbox.ru>
2019-11-05 10:50:08 +0300

    Рефакторинг variant_utils

--- variant_utils.h
+++ variant_utils.h
@@ -10,16 +10,22 @@ template<class T> struct always_false : std::false_type {};
 template<class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
 template<class... Ts> overloaded(Ts...) -> overloaded<Ts...>;
 
-#define BEGIN_VARIANT(element) \
-std::visit([&](auto &element) { \
+template<typename Visitor, typename Variant>
+constexpr decltype(auto)
+visit_impl(Variant&& variant, Visitor&& visitor) {
+    return std::visit(std::forward<Visitor>(visitor), std::forward<Variant>(variant));
+}
+
+#define BEGIN_VARIANT(variant, element) \
+common::visit_impl(variant, [&](auto &element) { \
 using ElementType = std::decay_t<decltype(element)>; \
 if constexpr (false) { \
 
-#define END_VARIANT(variants) \
+#define END_VARIANT() \
 } else { \
     static_assert(common::always_false<ElementType>::value, "non-exhaustive visitor!"); \
 } \
-}, variants);
+});
 
 #define CASE_VARIANT(type) \
 } else if constexpr (std::is_same_v<type, ElementType>) {
